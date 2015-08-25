47adfe8af7d8cfa04aea530d3d59ca1d75f6daa3
Dennis Dryden <dennis@brandwatch.com>
2015-08-25 12:51:17 +0100

    Add a default gateway and DNS server information.

--- ansible/roles/deploy-base/files/setup-interfaces.sh
+++ ansible/roles/deploy-base/files/setup-interfaces.sh
@@ -9,11 +9,17 @@ function add_interface ()
         network=$2
         host=$3
 	mtu=$4
+	if [ ! -z $5 ]; then primary_interface=1; fi
         printf "\nauto $interface\n"
         printf "iface $interface inet static\n"
         printf "\taddress $network.$host\n"
         printf "\tnetwork $network.0\n"
         printf "\tnetmask 255.255.255.0\n"
+	if [ ! -z primary_interface ]; then 
+		printf "\tgateway $network.254\n"
+		printf "\tdns-nameservers 10.2.1.17 10.2.2.2 10.2.2.3\n"
+		printf "\tdns-search brighton.brandwatch.net brandwatch.net\n"
+	fi
 }
 
 echo "Updating interfaces"
@@ -23,9 +29,8 @@ network_node=$(echo $current_ip | awk -F. '{ print $4 }')
 echo "current_ip = $current_ip"
 echo "Network node address = $network_node"
 
-
 printf "\n\nauto lo\niface lo inet loopback\n" > /etc/network/interfaces
-add_interface "p4p1" "10.2.133" $network_node "1500" >> /etc/network/interfaces
+add_interface "p4p1" "10.2.133" $network_node "1500" "10.2.133.254" >> /etc/network/interfaces
 printf "\nauto p4p2\niface p4p2 inet manual\n\tup ip link set dev p4p2 up\n\tdown ip link set dev p4p2 down\n" >> /etc/network/interfaces 
 add_interface "em1" "10.2.135" $network_node "9000" >> /etc/network/interfaces
 add_interface "em2" "10.2.136" $network_node "9000" >> /etc/network/interfaces
