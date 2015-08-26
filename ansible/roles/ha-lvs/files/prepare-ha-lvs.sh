2a0e518d0b0b4446a541cbd65653154e15a60bdb
Ben Greco <bengreco@linux.com>
2015-08-26 20:46:49 +0000

    Moved HA deployment on

--- ansible/roles/ha-lvs/files/prepare-ha-lvs.sh
+++ ansible/roles/ha-lvs/files/prepare-ha-lvs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 target_device="vdb"
 vg_name="drbd"
-mysql_capacity="5G"
-rabbit_capacity="4G"
+mysql_capacity="10G"
+rabbit_capacity="15G"
 
 vgcreate "$vg_name" "/dev/$target_device" &>/dev/null
 lvcreate "$vg_name" -n mysql -L"$mysql_capacity" &>/dev/null
