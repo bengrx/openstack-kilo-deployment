639a8b1cd5822264d33f37c83df200145efe9cf2
Ben Greco <bengreco@linux.com>
2015-08-31 13:54:16 +0000

    Fixed primer for ooo

--- primer/deploy-base/files/config-interfaces-vm.sh
+++ primer/deploy-base/files/config-interfaces-vm.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+if [ -z $4 ];then
+
+  echo -e "Interfaces must be specified: man tun ext stg"
+  exit 1
+fi
+
 for interface in $(ifconfig -a | grep -Eo "eth[0-3]" | tr '\n' ' ')
 do
-  if [ "$interface" != "eth3" ];then
+  if [ "$interface" != "$3" ];then
 
     echo -e "auto $interface\niface $interface inet dhcp" >/etc/network/interfaces.d/$interface.cfg
+
   else
     echo -e "auto $interface\niface $interface inet manual\n  up ip link set dev $interface up\n  down ip link set dev $interface down" >/etc/network/interfaces.d/$interface.cfg
   fi
